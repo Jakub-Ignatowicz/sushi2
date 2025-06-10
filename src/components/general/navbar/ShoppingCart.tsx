469d8459527b7d7d937e5fa45e4cd31ebebb836c
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-06-10 20:11:52 +0200

    feat(card): add items count

--- src/components/general/navbar/ShoppingCart.tsx
+++ src/components/general/navbar/ShoppingCart.tsx
@@ -1,7 +1,8 @@
 "use client";
+import { cn } from "@/lib/utils";
 import { CartItem } from "@/types";
 import { useEffect, useState } from "react";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaShoppingBasket } from "react-icons/fa";
 
 export default function ShoppingCart() {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
@@ -12,8 +13,17 @@ export default function ShoppingCart() {
   }, []);
 
   return (
-    <div>
-      <FaShoppingCart />
+    <div className="relative border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full">
+      <FaShoppingBasket size={28} />
+
+      <div
+        className={cn(
+          "absolute top-0 right-0 rounded-full bg-additional text-sm w-[20px] h-[20px] flex items-center justify-center translate-x-1/4 -translate-y-1/4",
+          cartItems.length == 0 ? "hidden" : "",
+        )}
+      >
+        {cartItems.length}
+      </div>
     </div>
   );
 }
