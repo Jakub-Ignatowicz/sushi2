92f32a812f7a11f4d2b0f0bae8f8c86b4a7da920
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-06-14 19:58:34 +0200

    feat(type): add product type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,4 +1,23 @@
-export interface CartItem {
+export type CartItem = {
   id: number;
   amount: number;
-}
+};
+
+export type Product = {
+  id: string;
+  name: string;
+  price: number;
+  isAvailable: boolean;
+  isVisible: boolean;
+  imagePath: string;
+  amount: number;
+  amountUnit: string;
+  productItems?: [
+    {
+      id: string;
+      description: string;
+      number: number;
+      numberSuffix: string;
+    },
+  ];
+};
