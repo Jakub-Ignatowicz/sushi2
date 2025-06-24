e763558f132be6e0dbeb7dd56cdba235f0b881f4
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-06-24 19:10:14 +0200

    build: accept backend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:5152/api/:path*", // proxy do backendu
+      },
+    ];
+  },
   images: {
     remotePatterns: [new URL("https://k84fneng6k.ufs.sh/**")],
   },
