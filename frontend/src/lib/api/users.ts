bba6f8e8dec24d268b75c389c7a18686cd2721d4
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-06-25 18:11:59 +0200

    feat(api): implement createUser

--- frontend/src/lib/api/users.ts
+++ frontend/src/lib/api/users.ts
@@ -1,2 +1,7 @@
+import { PostUser } from "@/types/api";
+import { fetchApi } from ".";
 
-
+export const createUser = async (user: PostUser) =>
+  await fetchApi<string>("/users", "POST", {
+    body: JSON.stringify(user),
+  });
