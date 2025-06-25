8f9c31a521c99a024ffb3a8fc74c57d71613cd59
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-06-25 18:10:49 +0200

    refactor(fetchApi): push errors to toaster

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -1,12 +1,14 @@
 import { ErrorResponse } from "@/types/api";
+import { toast } from "sonner";
 
 const BASE_URL = "http://localhost:5152/api";
 
+// TODO; frontend middlewear
 export const fetchApi = async <T>(
   endpoint: string,
   requestType: "GET" | "POST" | "PATCH" | "PUT" = "GET",
   options?: RequestInit,
-): Promise<T | ErrorResponse> => {
+): Promise<T | undefined> => {
   try {
     const normalizedEndpoint = endpoint.replace(/^\/+/, "");
     const res = await fetch(`${BASE_URL}/${normalizedEndpoint}`, {
@@ -18,20 +20,16 @@ export const fetchApi = async <T>(
     });
 
     if (!res.ok) {
-      const errorData = (await res.json()) as ErrorResponse;
-      return {
-        status: res.status,
-        errors: errorData.errors || ["Unknown error"],
-      };
+      toast.error(`Niestety wystąpił błąd: ${res.status}`);
+      return;
     }
 
     const data = (await res.json()) as T;
     return data;
   } catch (err) {
     console.error(err);
-    return {
-      status: 500,
-      errors: [err instanceof Error ? err.message : "Unexpected error"],
-    };
+    toast.error("Niestety wystąpił błąd: 500", {
+      description: [err instanceof Error ? err.message : "Unexpected error"],
+    });
   }
 };
