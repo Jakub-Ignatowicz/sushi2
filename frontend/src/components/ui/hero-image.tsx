3cce67cb3f81fc70e1205e76a97a55bf54d2f0b5
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-06-27 22:35:45 +0200

    feat(components): add resposivness to hero-image

--- frontend/src/components/ui/hero-image.tsx
+++ frontend/src/components/ui/hero-image.tsx
@@ -5,16 +5,71 @@ import { motion } from "framer-motion";
 
 export default function HeroImage() {
   return (
-    <>
+    <div>
+      <div className="hidden xl:inline">
+        <motion.div
+          initial={{ x: "100%" }}
+          animate={{ x: "40%" }}
+          transition={{
+            duration: 1.5,
+            delay: 0.1,
+            ease: [0.25, 0.8, 0.25, 1],
+          }}
+          className="absolute right-0 top-0 hidden xl:flex"
+        >
+          <motion.div
+            animate={{ rotate: 360 }}
+            transition={{
+              repeat: Infinity,
+              duration: 240,
+              ease: "linear",
+            }}
+            className="w-[850px] h-[850px]"
+          >
+            <Image src={"/icon.svg"} fill alt="logo" />
+          </motion.div>
+        </motion.div>
+
+        <motion.div
+          initial={{ x: "100%" }}
+          animate={{ x: "15%" }}
+          transition={{
+            duration: 1.8,
+            delay: 0.3,
+            ease: [0.25, 0.8, 0.25, 1],
+          }}
+          className="absolute right-0 top-0 hidden xl:flex"
+        >
+          <motion.div
+            animate={{
+              y: [-5, 5, -5],
+            }}
+            transition={{
+              duration: 6,
+              repeat: Infinity,
+              ease: "easeInOut",
+            }}
+            className="w-[850px] h-[850px]"
+          >
+            <Image
+              src={"/chopsticks.webp"}
+              width={850}
+              height={850}
+              alt="chopsticks"
+            />
+          </motion.div>
+        </motion.div>
+      </div>
+
       <motion.div
-        initial={{ x: "100%", opacity: 0 }}
-        animate={{ x: "33%", opacity: 1 }}
+        className="absolute left-1/2 top-1/4 -translate-x-1/2 -translate-y-1/4  xl:hidden "
+        initial={{ y: "200%" }}
+        animate={{ y: "0%" }}
         transition={{
-          duration: 1.5,
-          delay: 0.1,
+          duration: 1.8,
+          delay: 0.3,
           ease: [0.25, 0.8, 0.25, 1],
         }}
-        className="absolute right-0 top-0 hidden xl:flex"
       >
         <motion.div
           animate={{ rotate: 360 }}
@@ -23,41 +78,11 @@ export default function HeroImage() {
             duration: 240,
             ease: "linear",
           }}
-          className="w-[850px] h-[850px]"
+          className="w-[500px] h-[500px]"
         >
-          <Image src={"/icon.svg"} width={850} height={850} alt="logo" />
+          <Image src="/icon.svg" width={500} height={500} alt="logo" />
         </motion.div>
       </motion.div>
-
-      <motion.div
-        initial={{ x: "100%" }}
-        animate={{ x: "0%" }}
-        transition={{
-          duration: 1.8,
-          delay: 0.3,
-          ease: [0.25, 0.8, 0.25, 1],
-        }}
-        className="absolute right-0 top-0 hidden xl:flex"
-      >
-        <motion.div
-          animate={{
-            y: [-5, 5, -5], // góra -> środek -> dół -> środek
-          }}
-          transition={{
-            duration: 6,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-          className="w-[850px] h-[850px]"
-        >
-          <Image
-            src={"/chopsticks.webp"}
-            width={850}
-            height={850}
-            alt="chopsticks"
-          />
-        </motion.div>
-      </motion.div>
-    </>
+    </div>
   );
 }
