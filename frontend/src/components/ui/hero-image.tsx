68a19f8e0c0a82b4db58564118af4b215f8f562d
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-06-27 16:40:02 +0200

    feat(components): add animation to hero image

--- frontend/src/components/ui/hero-image.tsx
+++ frontend/src/components/ui/hero-image.tsx
@@ -1,24 +1,63 @@
 "use client";
 
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function HeroImage() {
   return (
     <>
-      <Image
-        src={"/icon.svg"}
-        width={850}
-        height={850}
-        className="absolute right-0 top-0 translate-x-1/3 hidden xl:flex"
-        alt="logo"
-      />
-      <Image
-        src={"/chopsticks.webp"}
-        width={850}
-        height={850}
+      <motion.div
+        initial={{ x: "100%", opacity: 0 }}
+        animate={{ x: "33%", opacity: 1 }}
+        transition={{
+          duration: 1.5,
+          delay: 0.1,
+          ease: [0.25, 0.8, 0.25, 1],
+        }}
         className="absolute right-0 top-0 hidden xl:flex"
-        alt="chopsticks"
-      />
+      >
+        <motion.div
+          animate={{ rotate: 360 }}
+          transition={{
+            repeat: Infinity,
+            duration: 240,
+            ease: "linear",
+          }}
+          className="w-[850px] h-[850px]"
+        >
+          <Image src={"/icon.svg"} width={850} height={850} alt="logo" />
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        initial={{ x: "100%" }}
+        animate={{ x: "0%" }}
+        transition={{
+          duration: 1.8,
+          delay: 0.3,
+          ease: [0.25, 0.8, 0.25, 1],
+        }}
+        className="absolute right-0 top-0 hidden xl:flex"
+      >
+        <motion.div
+          animate={{
+            y: [-5, 5, -5], // góra -> środek -> dół -> środek
+          }}
+          transition={{
+            duration: 6,
+            repeat: Infinity,
+            ease: "easeInOut",
+          }}
+          className="w-[850px] h-[850px]"
+        >
+          <Image
+            src={"/chopsticks.webp"}
+            width={850}
+            height={850}
+            alt="chopsticks"
+          />
+        </motion.div>
+      </motion.div>
     </>
   );
 }
