acde46e9796b85ffe3eb38608b2ed72f5be15eef
kawre <kenterq@gmail.com>
2025-07-06 16:08:34 +0200

    fix: separate prod/dev

--- .github/actions/deploy-to-vps/deploy.sh
+++ .github/actions/deploy-to-vps/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+PROJECT_NAME="prod"
 COMPOSE_FILES="-f docker-compose.yaml"
 
 if [ "$STAGE" != "production" ]; then
+	PROJECT_NAME="dev"
 	COMPOSE_FILES="$COMPOSE_FILES -f docker-compose.dev.yaml"
 fi
 
-docker compose $COMPOSE_FILES down
-docker compose $COMPOSE_FILES build
-docker compose $COMPOSE_FILES up -d --wait --wait-timeout 30
+docker compose -p $PROJECT_NAME $COMPOSE_FILES down
+docker compose -p $PROJECT_NAME $COMPOSE_FILES build
+docker compose -p $PROJECT_NAME $COMPOSE_FILES up -d --wait --wait-timeout 30
