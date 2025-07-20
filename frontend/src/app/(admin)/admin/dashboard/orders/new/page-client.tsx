ae5b230d7b56634713a67a50035743b264968432
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-07-20 20:20:46 +0200

    feat(admin/orders/new): add sound on pop up

--- frontend/src/app/(admin)/admin/dashboard/orders/new/page-client.tsx
+++ frontend/src/app/(admin)/admin/dashboard/orders/new/page-client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Order as OrderType } from "@/types/api";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getNewOrders } from "@/lib/api/orders";
 import { formatDate } from "@/lib/utils";
 import { PackageX } from "lucide-react";
@@ -13,16 +13,31 @@ const OrdersClientPage = () => {
   const [orders, setOrders] = useState<OrderType[] | null>(null);
   const [lastUpdate, setLastUpdate] = useState<Date>(new Date());
   const [isLive, setIsLive] = useState<boolean>(true);
+  const [firstLoad, setFirstLoad] = useState<boolean>(true);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const fetchOrders = async () => {
     try {
       const res = await getNewOrders();
+      let containsNew = false;
+      if (!firstLoad) {
+        for (let i = 0; i < res.length; i++) {
+          if (!orders?.some((order) => order.id === res[i].id)) {
+            containsNew = true;
+            break;
+          }
+        }
+      }
+      if (audioRef.current && !firstLoad && containsNew) {
+        audioRef.current.play();
+      }
       setOrders(res);
       setLastUpdate(new Date());
       setIsLive(true);
     } catch (error: any) {
       setIsLive(false);
     }
+    setFirstLoad(false);
   };
 
   useEffect(() => {
@@ -37,6 +52,7 @@ const OrdersClientPage = () => {
 
   return (
     <div className="flex flex-col gap-4 w-full">
+      <audio ref={audioRef} src="/bell.mp3"></audio>
       <OrdersTable columns={columns} data={orders} />
       <div className="mx-auto flex items-center gap-2 text-sm">
         {isLive ? (
